efa998bfbade684ef1f9ade8550e36b0c885224d
fatcatsnrats <77399589+fatcatsnrats@users.noreply.github.com>
2022-11-23 15:05:04 -0500

    Update Assignment12.py

--- Assignments/Assignment12.py
+++ Assignments/Assignment12.py
@@ -1,5 +1,5 @@
-# 0001 11/17/2022
-## Begin Omar A. Student here (11/17/2022)
+# 0003 11/23/2022
+## Begin Omar A. Student here (11/23/2022)
 
 employeeInfo = [1121, "Jackie Grainger", 22.22,
     1122, "Jignesh Thrakkar", 25.25,
@@ -18,11 +18,10 @@ companyRaises = []
 
 def dataSort(arr):
     employeeDict = {}
-    lineIndex = 0
     # Checks to see if employee is already in database.
     def checkDuplicate():
         if employeeDict.copy() in database:
-            return
+            pass
         else:
             tempInt = employeeDict.copy()['Employee ID:']
             # Checks to see if incomplete employee data already in database.
@@ -30,37 +29,59 @@ def dataSort(arr):
                 currentDict = database[i]
                 if currentDict['Employee ID:'] == tempInt:
                     database.pop(i)
-            database.append(employeeDict.copy())
+            return employeeDict.copy()
+            # database.append(employeeDict.copy())
 
-    # Adds entries from imported list into a dictionary which
-    # may later be appended to the 'database' list.
-    for x in range(len(arr)):
-        if isinstance(arr[x], bool):
-            employeeDict['(Unknown field):'] = arr[x]
-            lineIndex = 0
-            checkDuplicate()
-            employeeDict.clear()
-        elif isinstance(arr[x], int):
-            employeeDict['Employee ID:'] = arr[x]
-            lineIndex += 1
-        elif isinstance(arr[x], str):
-            employeeDict['Employee Name:'] = arr[x]
-            lineIndex += 1
-        elif isinstance(arr[x], float):
-            employeeDict['Employee Hourly Wage:'] = arr[x]
-            lineIndex += 1
+    # This is a generator that takes multiple entries from a list
+    # until it makes up on person and makes a dictionary with those values.
+    # It then yeilds the dictionary it created. It records how many entries
+    # its inputted since the last person it has created and stores it
+    # to 'lineIndex' then decides whether or not it has enough data
+    # to make up a person. It knows it has enough data when:
+    # "'lineIndex' is three and there IS NOT a boolean in the next list entry"
+    # and when: "'lineIndex' is greater than three".
+    def employeeGen(x):
+        lineIndex = 0
+        while lineIndex < 5:
+            # Adds entries from imported list into a dictionary which
+            # may later be appended to the 'database' list.
+            if isinstance(arr[x + lineIndex], bool):
+                employeeDict['(Unknown field):'] = arr[x + lineIndex]
+                lineIndex += 1
+            elif isinstance(arr[x + lineIndex], int):
+                employeeDict['Employee ID:'] = arr[x + lineIndex]
+                lineIndex += 1
+            elif isinstance(arr[x + lineIndex], str):
+                employeeDict['Employee Name:'] = arr[x + lineIndex]
+                lineIndex += 1
+            elif isinstance(arr[x + lineIndex], float):
+                employeeDict['Employee Hourly Wage:'] = arr[x + lineIndex]
+                lineIndex += 1
 
-        # Checks to see whether all employee information is in the dictionary.
-        # If it is, it will append the current dictionary to
-        # the 'database' list and clear the current dictionary.
-        # The try: and except: is for the end of the list.
-        try:
-            if ((lineIndex >= 3) and (not isinstance(arr[x+1], bool))):
-                lineIndex = 0
+            # Checks to see whether all employee information
+            # is in the dictionary. If it is, it will append
+            # the current dictionary to the 'database' list
+            # and clear the current dictionary.
+            # The try: and except: is for the end of the list.
+            try:
+                if (lineIndex > 3 or ((lineIndex == 3) 
+                    and (not isinstance(arr[x + lineIndex + 1], bool)))):
+                    
+                    lineIndex = 0
+                    checkDuplicate()
+                    yield employeeDict.copy()
+                    employeeDict.clear()
+            except:
                 checkDuplicate()
-                employeeDict.clear()
-        except:
-            checkDuplicate()
+                yield employeeDict.copy()
+                lineIndex = 100
+                
+    # Appends values to the database list using the employeeGen() generator.
+    try:
+        for count, x in enumerate(arr):
+            database.append(next(employeeGen(count)))
+    except:
+        pass
 
 def totalHourly():
     # Goes through the 'database' list calculating the total hourly rate
@@ -102,5 +123,5 @@ print(database, '\n')
 print(underpaidSalaries,'\n')
 print(companyRaises, '\n')
 
-# 0001 11/17/2022
-## END Omar A. Student here (11/17/2022)
+# 0003 11/23/2022
+## END Omar A. Student here (11/23/2022)

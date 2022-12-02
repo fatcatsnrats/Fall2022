acaf070b1bf1b76518cc92affd0d5f0d5c7cdcae
fatcatsnrats <77399589+fatcatsnrats@users.noreply.github.com>
2022-12-02 18:05:38 -0500

    Update Project 3.py

--- Project 3.py
+++ Project 3.py
@@ -40,14 +40,25 @@ def vigenereCipher():
     main()
 
 def caesarCipher():
+    shift = int(input('How many positions do you want to shift?\n'))
+    while shift > 26:
+            shift -= 26
+    text = input('Enter what you what to cipher.\n')
+    methodNum = 0
+    while methodNum == 0:
+        try:
+            methodNum = int(input('Do you want to use method 1 or method 2?'
+                ' (1 or 2)\n'))
+            if (methodNum > 2) or (methodNum < 0):
+                methodNum = 0
+        except:
+            print('Enter 1 or 2.')
+
     def method1():
         encryptedText = ''
         decryptedText = ''
-        shift = int(input('How many positions do you want to shift?\n'))
-        while shift > 26:
-            shift -= 26
 
-        text = input('Enter what you what to cipher.\n')
+        # Encryption
         for x in text:
             if not x == ' ':
                 charIndex = ord(x.lower()) + shift - 97
@@ -59,6 +70,7 @@ def caesarCipher():
         print(encryptedText)
         input('Press ENTER to decrypt.')
 
+        # Decryption
         for x in encryptedText:
             if not x == ' ':
                 charIndex = ord(x) - shift - 97
@@ -71,14 +83,40 @@ def caesarCipher():
 
     def method2():
         abcStr = ''
-        shiftedList = []
-        for x in abc:
-            abcStr += x
-        for x in range(3):
-            shiftedList = abc
-            shiftedList.append(shiftedList.pop(0))
+        shiftedStr = ''
+        eText = ''
+        dText = ''
         
-    method1()
+        # Generates the alphabet and appends it to 'abcStr'.
+        # Also generates 'shiftedStr'
+        for x in range(26):
+            abcStr += chr(97 + x)
+            abcIndex = 97 + x + shift
+            if abcIndex > 112:
+                abcIndex -= 26
+            shiftedStr += chr(abcIndex)
+
+        # Encryption
+        for x in text:
+            if x.isspace():
+                eText += x
+            else:
+                eText += shiftedStr[abcStr.find(x.lower())]
+        print(eText)
+
+        # Decryption
+        input('Press ENTER to decrypt.')
+        for x in eText:
+            if x.isspace():
+                dText += x
+            else:
+                dText += abcStr[shiftedStr.find(x.lower())]
+        print(dText)
+
+    if methodNum == 1:
+        method1()
+    else:
+        method2()
 
 caesarCipher()
 # vigenereCipher()

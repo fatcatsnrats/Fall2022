1ff6ce5d9d1546103626f40fb34d93b32f7801c4
fatcatsnrats <77399589+fatcatsnrats@users.noreply.github.com>
2022-12-11 16:40:56 -0500

    Update CSC1500FINAL.py

--- CSC1500 Final/CSC1500FINAL.py
+++ CSC1500 Final/CSC1500FINAL.py
@@ -84,6 +84,7 @@ def generate(objectType):
         'Breed': breed, 'Favorite Activities': favoriteActivities()}
     tempDict['Photo'] = photo()
 
+    database.append(tempDict)
     return tempDict
     
 
@@ -117,13 +118,16 @@ def checkForbidden(userInput):
 # This method acts as a query. It makes a new list of all results
 # and returns it.
 def databaseQuery(query):
+    results = []
     for x in range(len(database)):
         if query.lower() in str(database[x]['Favorite Activities']).lower():
-            yield database[x]
+            results.append(x)
+    print(results)
+    return results
 
 
 # This class acts as the controller and allows switching between frames.
-class DataBaseApp(tk.Tk):
+class DogTinderApp(tk.Tk):
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
         windowFrame = tk.Frame(self)
@@ -148,24 +152,32 @@ class DataBaseApp(tk.Tk):
         frame = self.frames[frameName]
         frame.tkraise()
 
+
+
 # This class controls the 'Home' frame and acts as the homepage. 
 class Home(tk.Frame):
-    def __init__(self, parent, controller):
+    def __init__(self, parent, controller, **photo):
         tk.Frame.__init__(self, parent)
         self.controller = controller
+        self.main()
 
-        mainFrame = tk.Frame(self, bg='#81b38e')
+    def main(self):
+        mainFrame = tk.Frame(self, bg='#81b38e', height=670)
+        buttonFrame = tk.Frame(self, bg='#81b38e', height=50)
         mainFrame.pack()
-        buttonFrame = tk.Frame(self, bg='#81b38e')
+        buttonFrame.pack()
 
         # if dogProfile is None:
         #     showinfo(title='Create a profile', message='You must create a profile first!', )
         #     lambda : controller.display('Profile')
-        while True:
-            # potentialDog = databaseQuery(dogProfile['Favorite Activities'][random.randint(0,2)])
-            # photo = ImageTk.PhotoImage(Image.open(dogProfile['Photo']))
-            photo = ImageTk.PhotoImage(Image.open((r'C:\Users\omara\Documents\Fall2022\CSC1500 Final\dog-cat-full-dataset\data\test\dogs\dog.0.jpg')))
-            label = tk.Label(mainFrame, image= photo)
+
+        tempDict = dogProfile['Favorite Activities']
+        potentialDog = databaseQuery(tempDict[random.randint(0,2)])
+        print(potentialDog)
+        for count, x in enumerate(potentialDog):
+            dog = database[x]
+            photo = ImageTk.PhotoImage(dog['Photo'])
+            label = tk.Label(self, image=photo)
             label.pack()
 
 
@@ -173,74 +185,70 @@ class Home(tk.Frame):
 
 
 
+        # canvasFrame1 = tk.Frame(mainFrame, bg='#81b38e')
+        # middleFrame = tk.Frame(mainFrame, bg='#81b38e')
+        # canvasFrame2 = tk.Frame(mainFrame, bg='#81b38e')
+        # labelFrame = tk.Frame(middleFrame, bg='#81b38e')
+        # buttonFrame = tk.Frame(middleFrame, bg='#81b38e')
+        # label1 = tk.Label(labelFrame,text="SafeZone©",
+        #     font=('calibre', 18), bg='#81b38e')
+        # label2 = tk.Label(labelFrame,text="by Forestview",
+        #     font=('calibre', 10), bg='#81b38e')
+        # label1.pack(side=tk.TOP)
+        # label2.pack(side=tk.TOP)
+        # canvas1 = tk.Canvas(canvasFrame1, height=120, width=260,
+        #     highlightthickness=1, highlightbackground="#266136", bg='#9bd1ae')
+        # canvas2 = tk.Canvas(canvasFrame2, height=120, width=260,
+        #     highlightthickness=1, highlightbackground="#266136", bg='#9bd1ae')
+        # canvas1.create_text(130, 67, font=('calibre', 7),
+        #     text="Press 'Import Data' to import a file to the database.\n"
+        #     "Press 'Add Data' to add entries to the database one by one.\n"
+        #     "Press 'Query Database' to search through the database.\n"
+        #     "Press 'Print Database' to print all the entries\n"
+        #     "   in the database.\n"
+        #     "Press 'Randomly create more data' to add random entries\n"
+        #     "   to the database.\n"
+        #     "Press 'Print Reversed Data' to print all the entries\n"
+        #     "   in the database.\n")
+        # canvas2.create_text(130, 60, font=('calibre', 7),
+        #     text="We are currently working on AWS cloud implimentation")
 
+        # button1 = tk.Button(buttonFrame, text="Import Data",
+        #     command=lambda: controller.display('ImportData'),
+        #     fg='#ffffff', bg='#266136')
+        # button2 = tk.Button(buttonFrame, text="Add Data",
+        #     command=lambda: controller.display('AddData'),
+        #     fg='#ffffff', bg='#266136')
+        # button3= tk.Button(buttonFrame, text='Query Database',
+        #     command=lambda: controller.display('QueryData'),
+        #     fg='#ffffff', bg='#266136')
+        # button4= tk.Button(buttonFrame, text='Print Database',
+        #     command=lambda: controller.display('LoadData'),
+        #     fg='#ffffff', bg='#266136')
+        # button5= tk.Button(buttonFrame, text='Randomly create more data',
+        #     command=lambda: controller.display("CreateData"),
+        #     fg='#ffffff', bg='#266136')
+        # button6= tk.Button(buttonFrame, text='Print Reversed Data',
+        #     command=lambda: controller.display('ReverseData'),
+        #     fg='#ffffff', bg='#266136')
 
+        # labelFrame.pack(side=tk.TOP, fill=tk.X, pady=10)
+        # buttonFrame.pack(side=tk.TOP, fill=tk.X)
 
+        # canvasFrame1.grid()
+        # middleFrame.grid(column=1, row=0)
+        # canvasFrame2.grid(column=2, row=0)
 
-        canvasFrame1 = tk.Frame(mainFrame, bg='#81b38e')
-        middleFrame = tk.Frame(mainFrame, bg='#81b38e')
-        canvasFrame2 = tk.Frame(mainFrame, bg='#81b38e')
-        labelFrame = tk.Frame(middleFrame, bg='#81b38e')
-        buttonFrame = tk.Frame(middleFrame, bg='#81b38e')
-        label1 = tk.Label(labelFrame,text="SafeZone©",
-            font=('calibre', 18), bg='#81b38e')
-        label2 = tk.Label(labelFrame,text="by Forestview",
-            font=('calibre', 10), bg='#81b38e')
-        label1.pack(side=tk.TOP)
-        label2.pack(side=tk.TOP)
-        canvas1 = tk.Canvas(canvasFrame1, height=120, width=260,
-            highlightthickness=1, highlightbackground="#266136", bg='#9bd1ae')
-        canvas2 = tk.Canvas(canvasFrame2, height=120, width=260,
-            highlightthickness=1, highlightbackground="#266136", bg='#9bd1ae')
-        canvas1.create_text(130, 67, font=('calibre', 7),
-            text="Press 'Import Data' to import a file to the database.\n"
-            "Press 'Add Data' to add entries to the database one by one.\n"
-            "Press 'Query Database' to search through the database.\n"
-            "Press 'Print Database' to print all the entries\n"
-            "   in the database.\n"
-            "Press 'Randomly create more data' to add random entries\n"
-            "   to the database.\n"
-            "Press 'Print Reversed Data' to print all the entries\n"
-            "   in the database.\n")
-        canvas2.create_text(130, 60, font=('calibre', 7),
-            text="We are currently working on AWS cloud implimentation")
+        # mainFrame.pack(side=tk.TOP)
 
-        button1 = tk.Button(buttonFrame, text="Import Data",
-            command=lambda: controller.display('ImportData'),
-            fg='#ffffff', bg='#266136')
-        button2 = tk.Button(buttonFrame, text="Add Data",
-            command=lambda: controller.display('AddData'),
-            fg='#ffffff', bg='#266136')
-        button3= tk.Button(buttonFrame, text='Query Database',
-            command=lambda: controller.display('QueryData'),
-            fg='#ffffff', bg='#266136')
-        button4= tk.Button(buttonFrame, text='Print Database',
-            command=lambda: controller.display('LoadData'),
-            fg='#ffffff', bg='#266136')
-        button5= tk.Button(buttonFrame, text='Randomly create more data',
-            command=lambda: controller.display("CreateData"),
-            fg='#ffffff', bg='#266136')
-        button6= tk.Button(buttonFrame, text='Print Reversed Data',
-            command=lambda: controller.display('ReverseData'),
-            fg='#ffffff', bg='#266136')
-
-        labelFrame.pack(side=tk.TOP, fill=tk.X, pady=10)
-        buttonFrame.pack(side=tk.TOP, fill=tk.X)
-
-        canvasFrame1.grid()
-        middleFrame.grid(column=1, row=0)
-        canvasFrame2.grid(column=2, row=0)
-
-        mainFrame.pack(side=tk.TOP)
-
-        button1.pack(side=tk.TOP, pady=10)
-        button2.pack(side=tk.TOP, pady=10)
-        button3.pack(side=tk.TOP, pady=10)
-        button4.pack(side=tk.TOP, pady=10)
-        button5.pack(side=tk.TOP, pady=10)
-        button6.pack(side=tk.TOP, pady=10)
-        canvas1.pack(padx=20)
-        canvas2.pack(padx=20)
+        # button1.pack(side=tk.TOP, pady=10)
+        # button2.pack(side=tk.TOP, pady=10)
+        # button3.pack(side=tk.TOP, pady=10)
+        # button4.pack(side=tk.TOP, pady=10)
+        # button5.pack(side=tk.TOP, pady=10)
+        # button6.pack(side=tk.TOP, pady=10)
+        # canvas1.pack(padx=20)
+        # canvas2.pack(padx=20)
 
 
 # This class controls the 'Messages' frame and acts as the homepage. 
@@ -249,7 +257,11 @@ class Messages(tk.Frame):
         tk.Frame.__init__(self, parent)
         self.controller = controller
 
+        mainFrame = tk.Frame(self, bg='#81b38e', height=670)
+        buttonFrame = tk.Frame(self, bg='#81b38e', height=50)
 
+        mainFrame.pack()
+        buttonFrame.pack()
 
 
 
@@ -274,14 +286,16 @@ class Profile(tk.Frame):
         tk.Frame.__init__(self, parent)
         self.controller = controller
 
-generate(10)
+generate(60)
 
-dogProfile = generate(1)
+dogProfile = {}
+dogProfile['Photo'] = r'C:\Users\omara\Documents\Fall2022\CSC1500 Final\dog-cat-full-dataset\data\test\dogs\dog.0.jpg'
+dogProfile["Favorite Activities"] = ['Swimming', 'Jumping', 'Napping']
 
 # dogProfile = create(1)
 
 # print(create(5))
 
 
-app = DataBaseApp()
+app = DogTinderApp()
 app.mainloop()
